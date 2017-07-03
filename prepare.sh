9249b343e7c129f176475705254e2f169d651d07
Marcus Grant <marcusfg@gmail.com>
2017-07-02 20:02:43 -0400

    Preparation script now links bash_profile properly

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,8 @@ dotfilePath=$(pwd)
 realConfigPath=$dotfilePath/.bashrc
 homePath=$HOME
 linkPath=$homePath/.bashrc
+profileLinkPath=$homePath/.bash_profile
+profilePath=$dotfilePath/.bash_profile
 
 echo "Preparing environment for BASH configuration."
 echo "Checking if previous .bashrc exists."
@@ -18,10 +20,24 @@ if [ -L $linkPath ]; then
 	rm $linkPath
 fi
 
+if [ -f $profileLinkPath ]; then
+    echo "Previous bash_profile exists, removing it."
+    rm $profileLinkPath
+fi
+
+if [ -f $profileLinkPath ]; then
+    echo "Previous bash_profile symlink exists, removing it."
+    rm $profileLinkPath
+fi
+
+
 echo "Creating symlink for .bashrc"
 echo "$linkPath -> $realConfigPath"
 ln -s $realConfigPath $linkPath
 
+echo "Creating symlink for .bash_profile"
+echo "$profileLinkPath -> $profilePath"
+ln -s $profilePath $profileLinkPath
 echo
 echo "Dotfile preperation complete!"
 exit 0
