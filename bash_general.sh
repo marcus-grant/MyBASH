d530689018be5168956e852dfdf3bde8adb9fb17
Marcus Grant <marcus.grant@patternbuffer.io>
2017-11-12 20:15:49 -0500

    now handles all miscallaneous bash settings

---
+++ bash_general.sh
@@ -0,0 +1,20 @@
+# 
+
+# Description:
+
+# # Here is where more general bashrc configurations that don't belong in any
+  # other partial config file.
+
+# Git Bash Completion
+# Will activate bash git completion if installed
+# via homebrew on macOS, or whatever linux package manager you use
+# NEEDS UPDATING TO WORK ON BOTH macOS & various Linux carnations
+#if [ -f `brew --prefix`/etc/bash_completion ]; then
+#  . `brew --prefix`/etc/bash_completion
+#fi
+
+# RVM - NEEDS AUDITING
+# Mandatory loading of RVM into the shell
+# This must be the last line of your bash_profile always
+#[[ -s "/Users/$USER/.rvm/scripts/rvm" ]] && source "/Users/$USER/.rvm/scripts/rvm"  # This loads RVM into a shell session.
+
