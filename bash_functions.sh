87332b5c1a4a1735cc7350315b8a804d409f3ff8
Marcus Grant <marcusfg@gmail.com>
2017-12-16 14:36:59 -0500

    node-permissions-whoami() sets global node_modules to whoami ownership

--- bash_functions.sh
+++ bash_functions.sh
@@ -239,6 +239,11 @@ view-markup ()
   function tmr()    { tmux rename-session -t $1 $2; }
   function tmlk()   { tmux list-keys; }
 
+# set node permissions to whoami
+node-permissions-whoami () {
+  sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+}
+
 
 
   # launch steam on arch
